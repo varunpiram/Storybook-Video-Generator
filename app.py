f9df709c4b8b0cb2587710c313222b768aaa0fe9
varunpiram <varunpiram@gmail.com>
2023-08-06 01:06:48 -0700

    Added redos

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def index():
 
 # Runs app locally
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port, debug=True)
--- handler.py
+++ handler.py
@@ -3,7 +3,7 @@ from video import VideoGenerator
 huey = RedisHuey()
 
 # Makes a huey task which calls on the generator to make a video
-@huey.task()
+@huey.task(retries=2, retry_delay=60)
 def generate(prompt, email):
     vg = VideoGenerator()
     vg.make(prompt, email)
